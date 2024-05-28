cf294a478247b1fcc39969e139e985b203cca3e7
khaledxab <khaledxab@gmail.com>
2024-05-28 15:34:59 +0200

    simplejwt done

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+# To fix the AlreadyRegistered error, modify the file `myapp/admin.py` as follows:
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+User = get_user_model()
+
+# Unregister the default User model and then register the custom User model
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer  
 
-# Create your views here.
+class UserProfileView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
